1e82344cf00ae89ddd4bbf42ff3455d08597a0e0
Joseph_Tully <tully4school@gmail.com>
2020-02-09 07:40:42 -0500

    tested global and scoped css in js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
+// import fetch from 'isomorphic-unfetch';
 
 // const PostLink = props => (
 //     <li>
@@ -23,31 +23,76 @@ import fetch from 'isomorphic-unfetch';
 //     );
 // }
 //////////////////FETCHING DATA///////////////
-const Index = props => (
-    <Layout>
-        <h1>Batman TV Shows</h1>
-        <ul>
-            {props.shows.map(show => (
-                <li key={show.id}>
-                    <Link href="/p/[id]" as={`/p/${show.id}`}>
-                        <a>{show.name}</a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    </Layout>
-)
+// const Index = props => (
+//     <Layout>
+//         <h1>Batman TV Shows</h1>
+//         <ul>
+//             {props.shows.map(show => (
+//                 <li key={show.id}>
+//                     <Link href="/p/[id]" as={`/p/${show.id}`}>
+//                         <a>{show.name}</a>
+//                     </Link>
+//                 </li>
+//             ))}
+//         </ul>
+//     </Layout>
+// )
 
-Index.getInitialProps = async function () {
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-    const data = await res.json();
+// Index.getInitialProps = async function () {
+//     const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+//     const data = await res.json();
 
-    console.log(`Show data fetched. Count: ${data.length}`);
-    console.log({ data })
+//     console.log(`Show data fetched. Count: ${data.length}`);
+//     console.log({ data })
 
-    return {
-        shows: data.map(entry => entry.show)
-    }
+//     return {
+//         shows: data.map(entry => entry.show)
+//     }
+// }
+
+// export default Index;
+
+/////////////////  ADDING STYLES WITH CSS IN JS (SCOPED CSS) ////////
+
+function getPosts() {
+    return [
+        { id: 'hellp-nextjs', title: 'Hello Next.js' },
+        { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+        { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+    ];
 }
 
-export default Index;
+const PostLink = ({ post }) => (
+    <li>
+        <Link href="/p/[id]" as={`/p/${post.id}`}>
+            <a>{post.title}</a>
+        </Link>
+    </li>
+);
+
+export default function Blog() {
+    return (
+        <Layout>
+            <h1>My Blog</h1>
+            <ul>
+                {getPosts().map(post => (
+                    <PostLink key={post.id} post={post} />
+                ))}
+            </ul>
+            <style jsx>{`
+            li {
+                list-style: none;
+                margin: 5px 0;
+            }
+            a {
+                text-decoration: none;
+                color: blue;
+                font-family: 'Arial';
+            }
+            a:hover {
+                opacity: 0.6;
+            }
+            `}</style>
+        </Layout>
+    );
+}
