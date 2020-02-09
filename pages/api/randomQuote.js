cf92167e942f536823f09d2c04dadd9ec8060ed8
Joseph_Tully <tully4school@gmail.com>
2020-02-09 08:44:20 -0500

    Fetching API Routes

--- pages/api/randomQuote.js
+++ pages/api/randomQuote.js
@@ -1,6 +1,17 @@
+//////////////// CREATING AN API ROUTE //////////////
+
+// export default (req, res) => {
+//     res.status(200).json({
+//         quote: 'Zeit is DOPE',
+//         author: 'Zakk Fleischmann'
+//     });
+// };
+
+/////////////FETCHING API ROUTES /////////////
+
+import quotes from '../../quotes.json';
+
 export default (req, res) => {
-    res.status(200).json({
-        quote: 'Zeit is DOPE',
-        author: 'Zakk Fleischmann'
-    });
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.status(200).json(quote);
 };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Layout from '../components/MyLayout';
-import Link from 'next/link';
+// import Layout from '../components/MyLayout';
+// import Link from 'next/link';
 // import fetch from 'isomorphic-unfetch';
 
 // const PostLink = props => (
@@ -54,45 +54,94 @@ import Link from 'next/link';
 
 /////////////////  ADDING STYLES WITH CSS IN JS (SCOPED CSS) ////////
 
-function getPosts() {
-    return [
-        { id: 'hellp-nextjs', title: 'Hello Next.js' },
-        { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-        { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-    ];
+// function getPosts() {
+//     return [
+//         { id: 'hellp-nextjs', title: 'Hello Next.js' },
+//         { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+//         { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+//     ];
+// }
+
+// const PostLink = ({ post }) => (
+//     <li>
+//         <Link href="/p/[id]" as={`/p/${post.id}`}>
+//             <a>{post.title}</a>
+//         </Link>
+//     </li>
+// );
+
+// export default function Blog() {
+//     return (
+//         <Layout>
+//             <h1>My Blog</h1>
+//             <ul>
+//                 {getPosts().map(post => (
+//                     <PostLink key={post.id} post={post} />
+//                 ))}
+//             </ul>
+//             <style jsx>{`
+//             li {
+//                 list-style: none;
+//                 margin: 5px 0;
+//             }
+//             a {
+//                 text-decoration: none;
+//                 color: blue;
+//                 font-family: 'Arial';
+//             }
+//             a:hover {
+//                 opacity: 0.6;
+//             }
+//             `}</style>
+//         </Layout>
+//     );
+// }
+
+//////////////// FETCHING API ROUTES /////////////////
+
+import useSWR from 'swr';
+import Layout from '../Components/MyLayout'
+
+function fetcher(url) {
+    return fetch(url).then(r => r.json());
 }
 
-const PostLink = ({ post }) => (
-    <li>
-        <Link href="/p/[id]" as={`/p/${post.id}`}>
-            <a>{post.title}</a>
-        </Link>
-    </li>
-);
+export default function Index() {
+    const { data, error } = useSWR('/api/randomQuote', fetcher);
+    //The following line has optional chaining, added in next.js v9.1.5,
+    // is the same as `data && data.author`
+    const author = data?.author;
+    let quote = data?.quote;
 
-export default function Blog() {
+    if (!data) quote = 'Loading...';
+    if (error) quote = 'Failed to fetch the quote.';
+
     return (
         <Layout>
-            <h1>My Blog</h1>
-            <ul>
-                {getPosts().map(post => (
-                    <PostLink key={post.id} post={post} />
-                ))}
-            </ul>
-            <style jsx>{`
-            li {
-                list-style: none;
-                margin: 5px 0;
+            <main className="center">
+                <div className="quote">{` \u201C${quote}\u201D`}</div>
+                {author && <span className="author">- {author}</span>}
+
+                <style jsx>{`
+            main {
+                width: 90%;
+                max-width: 900px;
+                margin: 300px auto;
+                text-align: center;
             }
-            a {
-                text-decoration: none;
-                color: blue;
-                font-family: 'Arial';
+            .quote {
+                font-family: cursive;
+                color: #e243de;
+                font-size: 24px;
+                padding-bottm: 10px;
             }
-            a:hover {
-                opacity: 0.6;
-            }
+            .author {
+                font-family: sans-serif;
+                color: #559832; 
+                font-size: 20px;
+}
             `}</style>
+            </main>
         </Layout>
-    );
+    )
 }
