90e138a6be694cf08cabaa61ecb06b160edf5378
Joseph_Tully <tully4school@gmail.com>
2020-02-09 09:09:57 -0500

    Fetching API Routes

--- pages/api/randomQuote.js
+++ pages/api/randomQuote.js
@@ -9,6 +9,13 @@
 
 /////////////FETCHING API ROUTES /////////////
 
+// import quotes from '../../quotes.json';
+
+// export default (req, res) => {
+//     const quote = quotes[Math.floor(Math.random() * quotes.length)];
+//     res.status(200).json(quote);
+// };
+
 import quotes from '../../quotes.json';
 
 export default (req, res) => {
